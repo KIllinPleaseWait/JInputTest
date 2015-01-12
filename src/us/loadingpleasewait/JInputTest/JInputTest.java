b89b6eca658c6332600406dc38ff338c0f3574e9
LoadingPleaseWait <michael@murphey.org>
2015-01-11 22:57:58 -0600

    add a bit to gitignore and main class

--- src/us/loadingpleasewait/JInputTest/JInputTest.java
+++ src/us/loadingpleasewait/JInputTest/JInputTest.java
@@ -1,8 +1,17 @@
 package us.loadingpleasewait.JInputTest;
 
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
+
 public class JInputTest {
 
 	public static void main(String[] args) {
+		Controller[] controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
+		
+		// just print the names of the controllers
+		for(Controller controller : controllers){
+			System.out.println(controller.getName());
+		}
 	}
 
 }
